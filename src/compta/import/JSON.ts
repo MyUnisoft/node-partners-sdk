b0703bfc3813b45d52dd5c026859c15556511bf9
SofianD <doual.sofian@hotmail.com>
2021-11-18 00:03:09 +0100

    feat: add JSON methods

---
+++ src/compta/import/JSON.ts
@@ -0,0 +1,48 @@
+// Import Third-party Dependencies
+import * as httpie from "@myunisoft/httpie";
+import { Windev } from "@myunisoft/tsd";
+
+// Import Internal Dependencies
+import { IDefaultOptions, setDefaultHeaderOptions } from "../../constants";
+
+export interface IEntryOptions extends IDefaultOptions {
+  body: Windev.Entry.NewEntry;
+}
+
+export interface IDirectEntryOptions extends IEntryOptions {
+  params: {
+    /**
+     * E = ecriture
+     *
+     * O = ocr
+     */
+    type: "e" | "o"
+  };
+}
+
+export async function sendDirectEntry(options: IDirectEntryOptions) {
+  const endpoint = new URL("/api/v1/entry");
+  endpoint.searchParams.set("type", options.params.type);
+
+  options.header.contentType = "application/json";
+
+  const { data } = await httpie.post(endpoint, {
+    ...setDefaultHeaderOptions(options.header),
+    body: options.body
+  });
+
+  return data;
+}
+
+export async function sendEntryPending(options: IEntryOptions) {
+  const endpoint = new URL("/api/v1/entry/temp");
+
+  options.header.contentType = "application/json";
+
+  const { data } = await httpie.post(endpoint, {
+    ...setDefaultHeaderOptions(options.header),
+    body: options.body
+  });
+
+  return data;
+}
