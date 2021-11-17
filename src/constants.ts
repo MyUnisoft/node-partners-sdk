8da16573ffa3a377005ade88b9686a5c79370229
SofianD <doual.sofian@hotmail.com>
2021-11-17 23:17:18 +0100

    feat: update and add methods to set the req params

--- src/constants.ts
+++ src/constants.ts
@@ -7,27 +7,64 @@ export const BASE_API_URL = "https://app.myunisoft.fr/";
 /**
  * @description Default interface for all requests options
  */
-export interface IDefaultOptions {
+export interface IDefaultHeaderOptions {
   /** API Bearer Token */
   accessToken: string;
 
   /** Company (dossier de production) id */
   societyId?: string | number;
+
+  contentType?: "application/octect" | "application/json"
+}
+
+export interface IDefaultOptions {
+  header: IDefaultHeaderOptions
 }
 
 /**
  * @description Default httpie options with all required headers
  */
-export function getDefaultOptions(options: IDefaultOptions) {
+export function setDefaultHeaderOptions(options: IDefaultHeaderOptions) {
   const headers = {
     "X-Third-Party-Secret": secret.get()
   };
+
   if ("societyId" in options) {
-    headers["society-id"] = String(options.societyId);
+    headers["society-id"] = options.societyId;
+  }
+
+  if ("contentType" in options) {
+    headers["Content-Type"] = options.contentType;
   }
 
   return {
     authorization: options.accessToken,
     headers
   };
+}
+
+// export function setSearchParams(url: URL, options: any, params: string[]) {
+//   for (const param of params) {
+//     if (param in options) {
+//       if (typeof options[param] === "object") {
+//         options[param] = JSON.stringify(options[param]);
+//       }
+//       url.searchParams.set(param, options[param]);
+//     }
+//   }
+
+//   return;
+// }
+
+export function setSearchParams(url: URL, options: any, excludeParams?: string[]) {
+  for (const option in options) {
+    if (option in options && !excludeParams?.includes(option)) {
+      if (typeof options[option] === "object") {
+        options[option] = JSON.stringify(options[option]);
+      }
+      url.searchParams.set(option, options[option]);
+    }
+  }
+
+  return;
 }
