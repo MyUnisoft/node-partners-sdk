f9335b5e9b4c96281ddf7eabdd74fd83060b8bdd
SofianD <doual.sofian@hotmail.com>
2021-11-17 23:19:30 +0100

    refactor:  set up the right interface and the right method

--- src/compta/account.ts
+++ src/compta/account.ts
@@ -3,25 +3,25 @@ import * as httpie from "@myunisoft/httpie";
 import { Windev } from "@myunisoft/tsd";
 
 // Import Internal Dependencies
-import { BASE_API_URL, IDefaultOptions, getDefaultOptions } from "../constants";
+import { BASE_API_URL, IDefaultHeaderOptions, setDefaultHeaderOptions } from "../constants";
 
-export async function getAll(options: IDefaultOptions) {
+export async function getAll(options: IDefaultHeaderOptions) {
   const endpoint = new URL("/api/v1/account", BASE_API_URL);
   endpoint.searchParams.set("mode", "1");
 
   const { data } = await httpie.get<Windev.Account.SimplifiedAccount[]>(endpoint, {
-    ...getDefaultOptions(options)
+    ...setDefaultHeaderOptions(options)
   });
 
   return data;
 }
 
-export async function getAllDetailed(options: IDefaultOptions) {
+export async function getAllDetailed(options: IDefaultHeaderOptions) {
   const endpoint = new URL("/api/v1/account", BASE_API_URL);
   endpoint.searchParams.set("mode", "2");
 
   const { data } = await httpie.get<Windev.Account.DetailedAccounts>(endpoint, {
-    ...getDefaultOptions(options)
+    ...setDefaultHeaderOptions(options)
   });
 
   return data;
