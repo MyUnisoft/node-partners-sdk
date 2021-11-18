5cd913d34f8e3d0e9dc7a4a27c78554b3a6b987b
SofianD <doual.sofian@hotmail.com>
2021-11-18 23:17:31 +0100

    feat: implement "Export"

--- src/compta/index.ts
+++ src/compta/index.ts
@@ -8,5 +8,6 @@
 // Exports sub modules
 export * as account from "./account";
 export * as society from "./society";
-export * as importation from "./import/index";
 export * as firm from "./firm";
+export * as importation from "./import/index";
+export * as exportation from "./export/index";
