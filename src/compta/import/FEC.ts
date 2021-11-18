ffdb3c2601e7fd11068454c7a70c30d5152c4520
SofianD <doual.sofian@hotmail.com>
2021-11-18 22:41:34 +0100

    feat: add FEC file

---
+++ src/compta/import/FEC.ts
@@ -0,0 +1,35 @@
+// Import Third-party Dependencies
+import * as httpie from "@myunisoft/httpie";
+
+// Import Internal Dependencies
+import { IDefaultOptions, setDefaultHeaderOptions } from "../../constants";
+
+export interface ISendFECOptions extends IDefaultOptions {
+  params: {
+    exerciceId: number;
+    filename: string;
+
+    /**
+     * 0 = check if there is entries on the exercise (returns an error message).
+     *
+     * 1 = no verification, the data is added to the existing exercise.
+     *
+     * 2 = delete the entries if present on the exercise.
+     */
+    type: 0 | 1 | 2;
+  };
+  body: Buffer | string;
+}
+
+export async function sendTRA(options: ISendFECOptions) {
+  const endpoint = new URL("/api/v1/fec");
+
+  options.header.contentType = "application/octet-stream";
+
+  const { data } = await httpie.get<{status: string}>(endpoint, {
+    ...setDefaultHeaderOptions(options.header)
+  });
+
+  return data;
+}
+
