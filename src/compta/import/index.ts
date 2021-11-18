30daf8c9f4a4a8eeba2909657e4f76cbc8dccaf0
SofianD <doual.sofian@hotmail.com>
2021-11-18 22:24:41 +0100

    feat: add method to add an attachment to an entry

--- src/compta/import/index.ts
+++ src/compta/import/index.ts
@@ -1,1 +1,36 @@
+// Import Third-party Dependencies
+import * as httpie from "@myunisoft/httpie";
+import { Windev } from "@myunisoft/tsd";
+
+// Import Internal Dependencies
+import { BASE_API_URL, IDefaultOptions, setDefaultHeaderOptions } from "../../constants";
+
 export * as ocr from "./ocr";
+export * as factureX from "./FacturX";
+export * as JSON from "./JSON";
+
+export interface IAddAttachmentToEntryOptions extends IDefaultOptions {
+  params: {
+    location: "ENTRIES" | "ENTRIES_TEMP";
+    objectId: number;
+    filenameExtension: string;
+    // ???
+    // typeResult: 1;
+  };
+}
+
+export async function addAttachmentToEntry(options: IAddAttachmentToEntryOptions) {
+  const endpoint = new URL("/api/v1/document/add_all_types", BASE_API_URL);
+  endpoint.searchParams.set("location", options.params.location);
+  endpoint.searchParams.set("object_id", String(options.params.objectId));
+  endpoint.searchParams.set("filename_extension", options.params.filenameExtension);
+  endpoint.searchParams.set("type_result", "1");
+
+  options.header.contentType = "application/octet-stream";
+
+  const { data } = await httpie.get(endpoint, {
+    ...setDefaultHeaderOptions(options.header)
+  });
+
+  return data;
+}
