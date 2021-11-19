855d41fe99f8250f2027838b5be926d93aaa17ff
SofianD <doual.sofian@hotmail.com>
2021-11-19 19:09:46 +0100

    feat: add findRelateddEntries

--- src/compta/account.ts
+++ src/compta/account.ts
@@ -3,9 +3,18 @@ import * as httpie from "@myunisoft/httpie";
 import { Windev } from "@myunisoft/tsd";
 
 // Import Internal Dependencies
-import { BASE_API_URL, IDefaultHeaderOptions, setDefaultHeaderOptions } from "../constants";
+import {
+  BASE_API_URL,
+  firmAccessThrowWithoutSociety,
+  IDefaultHeaderOptions,
+  IDefaultOptions,
+  setDefaultHeaderOptions,
+  setSearchParams
+} from "../constants";
 
 export async function getAll(options: IDefaultHeaderOptions) {
+  firmAccessThrowWithoutSociety(options);
+
   const endpoint = new URL("/api/v1/account", BASE_API_URL);
   endpoint.searchParams.set("mode", "1");
 
@@ -17,6 +26,8 @@ export async function getAll(options: IDefaultHeaderOptions) {
 }
 
 export async function getAllDetailed(options: IDefaultHeaderOptions) {
+  firmAccessThrowWithoutSociety(options);
+
   const endpoint = new URL("/api/v1/account", BASE_API_URL);
   endpoint.searchParams.set("mode", "2");
 
@@ -27,4 +38,56 @@ export async function getAllDetailed(options: IDefaultHeaderOptions) {
   return data;
 }
 
-// TODO: findOrCreate, findRelatedEntries
+export interface IFindRelatedEntriesOptions extends IDefaultOptions {
+  params: {
+    accountNo: number;
+    accountId: number;
+    limit?: number;
+
+    /**
+     * Starting rows (Default 0).
+     */
+    offset?: number;
+
+    /**
+     * Optionnal date ranges but if it's activated start_date and end_date must be filled.
+     */
+    searchDate?: string;
+
+    sort?: {
+      direction: "desc" | "asc";
+      column: "credit" | "debit" | "date" | "label" | "lettrage" | "piece" | "piece2";
+    };
+
+    /**
+     * If lettering param not filled all entries are retrieved.
+     *
+     * True = entries filtered by lettering not empty.
+     * False = entries filtered by lettering empty.
+     */
+    lettering?: boolean;
+
+    // ???
+    prevnext?: "previous" | "next";
+  }
+}
+
+export async function findRelatedEntries(options: IFindRelatedEntriesOptions) {
+  firmAccessThrowWithoutSociety(options.header);
+
+  const endpoint = new URL("/api/v1/account/entries", BASE_API_URL);
+  setSearchParams(endpoint, options.params, {
+    accountNo: "account_no",
+    accountId: "account_id",
+    searchDate: "search_date"
+  });
+
+  // Vérifier le type de retour.
+  const { data } = await httpie.get<Windev.Account.AccountEntries>(endpoint, {
+    ...setDefaultHeaderOptions(options.header)
+  });
+
+  return data;
+}
+
+// TODO: findOrCreate
